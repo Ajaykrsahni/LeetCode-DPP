90f013da40d424a36c88025e2f0c5a32c76e6ce6
Ajaykrsahni <161690027+Ajaykrsahni@users.noreply.github.com>
2026-05-26 23:52:49 +0530

    Time: 1 ms (100%), Space: 43.3 MB (95.05%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numberOfSpecialChars(String word) {
+        int lower = 0;
+        int upper = 0;
+        for(char ch : word.toCharArray()) {
+            if(Character.isLowerCase(ch)) {
+                lower |= (1 << (ch - 'a'));
+            }
+            else {
+                upper |= (1 << (ch - 'A'));
+            }
+        }
+        int common = lower & upper;
+        return Integer.bitCount(common);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
