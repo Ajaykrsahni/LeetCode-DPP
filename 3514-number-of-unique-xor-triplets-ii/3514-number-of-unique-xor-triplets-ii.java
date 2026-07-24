0b2016f15c012b7d4e33c48c9c60b6f8b48ea16e
Ajaykrsahni <161690027+Ajaykrsahni@users.noreply.github.com>
2026-07-24 23:32:03 +0530

    Time: 178 ms (98.18%), Space: 47 MB (49.09%) - LeetHub - Time: O(n^2), Space: O(1)

---
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int uniqueXorTriplets(int[] nums) {
+        final int MAX_XOR = 2048;
+        boolean[] pairXor = new boolean[MAX_XOR];
+        boolean[] tripletXor = new boolean[MAX_XOR];
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                pairXor[nums[i] ^ nums[j]] = true;
+            }
+        }
+        for (int x = 0; x < MAX_XOR; x++) {
+            if (!pairXor[x]) continue;
+            for (int v : nums) {
+                tripletXor[x ^ v] = true;
+            }
+        }
+        int count = 0;
+        for (boolean exists : tripletXor) {
+            if (exists) count++;
+        }
+        return count;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
