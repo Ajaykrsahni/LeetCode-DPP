424c3f3ea562c8119842fa5950bcb1df07b81228
Ajaykrsahni <161690027+Ajaykrsahni@users.noreply.github.com>
2026-08-03 00:22:47 +0530

    Time: 0 ms (100%), Space: 43 MB (23.58%) - LeetHub - Time: O(n^2), Space: O(n)

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        if (n % 2 == 0) return true;
+        int[] dp = nums.clone();
+        for (int i = n - 2; i >= 0; --i) {
+            for (int j = i + 1; j < n; ++j) {
+                dp[j] = Math.max(nums[i] - dp[j], nums[j] - dp[j - 1]);
+            }
+        }
+        return dp[n - 1] >= 0;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
