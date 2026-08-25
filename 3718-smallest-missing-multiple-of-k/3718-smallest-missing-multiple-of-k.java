be22113dd239fa9e383e06f7de9188834ab18b70
Ajaykrsahni <161690027+Ajaykrsahni@users.noreply.github.com>
2026-08-25 22:31:52 +0530

    Time: 2 ms (68.2%), Space: 45.7 MB (6.83%) - LeetHub

---
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+        Set<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            seen.add(num);
+        }
+        int cur = k;
+        while (seen.contains(cur)) {
+            cur += k;
+        }
+        return cur;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
