57929f3ca1a8cb35de3473afa68c3ce1c115c6ff
Ajaykrsahni <161690027+Ajaykrsahni@users.noreply.github.com>
2026-08-29 23:42:48 +0530

    Time: 101 ms (45.45%), Space: 244.4 MB (9.09%) - LeetHub - Time: O(n log n), Space: O(n)

---
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] lexicographicallySmallestArray(int[] nums, int limit) {
+        int n = nums.length;
+        int[] sorted = nums.clone();
+        Arrays.sort(sorted);
+        Map<Integer, List<Integer>> group = new HashMap<>();
+        Map<Integer, Integer> groupId = new HashMap<>();
+        Map<Integer, Integer> pos = new HashMap<>();
+        int id = 1;
+        group.computeIfAbsent(id, k -> new ArrayList<>()).add(sorted[0]);
+        groupId.put(sorted[0], id);
+        for(int i = 1; i < n; i++){
+            if(sorted[i] - sorted[i - 1] > limit){
+                id++;
+            }
+            group.computeIfAbsent(id, k -> new ArrayList<>()).add(sorted[i]);
+            groupId.put(sorted[i], id);
+        }
+        for(int i = 0; i < n; i++){
+            int grp = groupId.get(nums[i]);
+            int p = pos.getOrDefault(grp, 0);
+            nums[i] = group.get(grp).get(p);
+            pos.put(grp, p + 1);
+        }
+        return nums;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
